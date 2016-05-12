4d8c4dba8223984373dc5d2f2ea4b116f6955e1c
TheNotary <no@email.plz>
2016-05-12 16:02:20 -0700

    s2: starting OO, but putting things in wrong spot

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,23 @@
 class PagesController < ApplicationController
   def home
-    @untouched = "Hello Gems"
-    @encrypted = encrypt(@untouched)
-    @decrypted = decrypt(@encrypted)
+    @message = KewlCrypto.new("Hello Gems")
   end
 
-  def encrypt(string)
-    Base64.encode64(string)
+  # Placement here of a class is an anti-pattern
+  class KewlCrypto
+    attr_reader :untouched
+
+    def initialize(string)
+      @untouched = string
+    end
+
+    def encrypted
+      Base64.encode64(@untouched)
+    end
+
+    def decrypted
+      Base64.decode64(@untouched)
+    end
   end
 
-  def decrypt(string)
-    Base64.decode64(string)
-  end
 end
