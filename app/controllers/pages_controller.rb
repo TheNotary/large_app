1e7254e193a79a6e222bbe252037ab9c4d94260b
TheNotary <no@email.plz>
2016-05-12 14:11:56 -0700

    s1: methods used

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
+    @untouched = "Hello Gems"
+    @encrypted = encrypt(@untouched)
+    @decrypted = decrypt(@encrypted)
+  end
+
+  def encrypt(string)
+    Base64.encode64(string)
+  end
+
+  def decrypt(string)
+    Base64.decode64(string)
   end
 end
